1b58209bc1d3859ca38ad0c37c0a600b8e8c3a23
Digithinktech <developer@digithinktech.com>
2025-12-09 14:23:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,18 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // IMPORTANT: This matches your new repository name
+      base: "/userrail/",
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
+        // This prevents crashes if you use 'process' variables in your code
+        'process.env': {},
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
